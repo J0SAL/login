b058df8548d257d393ccaf5b026d42fb74e2fccb
J0SAL <joyalmeida321@gmail.com>
2023-07-04 20:44:06 +0530

    implement logout function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,41 @@
+"use client";
+
+import Axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+import React from "react";
+
 function Profile() {
+  const router = useRouter();
+
+  const [loading, setLoading] = React.useState(false);
+
+  const onLogout = async () => {
+    try {
+      setLoading(true);
+      const res = await Axios.get("/api/user/logout");
+      toast.success("Logout Success");
+      console.log(res);
+      router.push("/login");
+    } catch (err: any) {
+      toast.error(err.message);
+      console.log(err.message);
+    } finally {
+      setLoading(false);
+      console.log("logout");
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-2xl">Profile</h1>
       <hr />
       <p>Welcome to Profile Page</p>
+      <button
+        onClick={onLogout}
+        className="rounded bg-red-500 text-white py-1 px-2"
+      >
+        {loading ? "Loading" : "Logout"}
+      </button>
     </div>
   );
 }
