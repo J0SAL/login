2b8e5318ded41e9218cc429278efda3153c7c4d5
J0SAL <joyalmeida321@gmail.com>
2023-07-06 00:07:57 +0530

    added route to clear user collection

---
+++ src/app/api/user/cleardb/route.ts
@@ -0,0 +1,14 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+connect()
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    try{
+        await User.deleteMany({})
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+    catch(err){
+        console.log(err)
+    }
+}
